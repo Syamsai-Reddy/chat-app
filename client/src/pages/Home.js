098df310c1d5e1c4f7d30da1f9277c3ccbfb16b0
Syamsai-Reddy <126492434+Syamsai-Reddy@users.noreply.github.com>
2025-08-07 12:07:49 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
   console.log('user',user)
   const fetchUserDetails = async()=>{
     try {
-        const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
+        const URL = `https://chat-app-backend-pearl-iota.vercel.app/api/user-details`
         const response = await axios({
           url : URL,
           withCredentials : true
