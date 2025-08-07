9a4fdc7b3b8be353e3eabd6902cb4509086420a7
Syamsai-Reddy <126492434+Syamsai-Reddy@users.noreply.github.com>
2025-08-07 12:06:08 +0530

    Update SearchUser.js

--- client/src/components/SearchUser.js
+++ client/src/components/SearchUser.js
@@ -13,7 +13,7 @@ const SearchUser = ({onClose}) => {
 
 
     const handleSearchUser = async()=>{
-        const URL = `${process.env.REACT_APP_BACKEND_URL}/api/search-user`
+        const URL = `https://chat-app-backend-pearl-iota.vercel.app/api/search-user`
         try {
             setLoading(true)
             const response = await axios.post(URL,{
